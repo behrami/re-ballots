7cfecc015609fbfbc52f751a8e31b5b587d4f724
behrami <behram1993@gmail.com>
2018-04-18 09:59:45 -0400

    fixed finished

--- re-ex.rb
+++ re-ex.rb
@@ -5,37 +5,37 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
            {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
            {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
 
-Smudge_points=0
-Tigger_points=0
-Simba_points=0
-Bella_points=0
-Lucky_points=0
-Boots_points=0
-Felix_points=0
+smudge_points=0
+tigger_points=0
+simba_points=0
+bella_points=0
+lucky_points=0
+boots_points=0
+felix_points=0
 
 ballots.each do |ballot|
   ballot.each do |num, name|
 
     if name == 'Smudge'
-      Smudge_points += 4-num
+      smudge_points += 4-num
     elsif name == 'Tigger'
-      Tigger_points += 4-num
+      tigger_points += 4-num
     elsif name == 'Simba'
-      Simba_points += 4-num
+      simba_points += 4-num
     elsif name == 'Bella'
-      Bella_points += 4-num
+      bella_points += 4-num
     elsif name == 'Lucky'
-      Lucky_points += 4-num
+      lucky_points += 4-num
     elsif name == 'Boots'
-      Boots_points += 4-num
+      boots_points += 4-num
     elsif name == 'Felix'
-      Felix_points += 4-num
+      felix_points += 4-num
     end
 
   end
 end
 
-total_points = {'Smudge' =>Smudge_points, 'Tiger' => Tigger_points, 'Simba' => Simba_points, 'Bella'=>Bella_points, 'Luck'=>Lucky_points, 'Boots'=>Boots_points, 'Felix'=>Felix_points }
+total_points = {'Smudge' =>smudge_points, 'Tiger' => tigger_points, 'Simba' => simba_points, 'Bella'=>bella_points, 'Luck'=>lucky_points, 'Boots'=>boots_points, 'Felix'=>felix_points }
 
 max = total_points.values.max
 
